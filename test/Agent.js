a6187a42ec972f789b0a06c7d9efb548355a58b9
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-09-25 12:09:47 +0200

    more test coverage using test data

--- test/Agent.js
+++ test/Agent.js
@@ -21,8 +21,8 @@ const fs = require('fs')
 const util = require('util')
 const readFile = util.promisify(fs.readFile)
 const ChainBlockHashes = artifacts.require('../contracts/ChainBlockHashes.sol')
-
-// const expectThrow = require('./helpers/expectThrow.js')
+const expectThrow = require('./helpers/expectThrow.js')
+const md5 = require('md5')
 // const should = require('chai') // eslint-disable-line no-unused-vars
 //   .use(require('chai-as-promised'))
 //   .should()
@@ -30,11 +30,20 @@ const ChainBlockHashes = artifacts.require('../contracts/ChainBlockHashes.sol')
 const chainCount = 4
 const fixtureLines = 30
 let fixtures = []
+// For the purposes of this test we're pretending these contracts
+// are deployed on four different chains
 let contracts = []
+
 const mainnetGenesis = '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
 const mordenGenesis = '0x0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303'
 const ropstenGenesis = '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d'
 const kovanGenesis = '0xa3c565fc15c7478862d50ccd6561e3c06b24cc509bf388941c25ea985ce32cb9'
+
+// test data was created in Google Sheets and used the venerable MD5 hash.
+function fakeHash (value) {
+  let hash = md5(String(value))
+  return '0x' + hash + hash
+}
 
 contract('ChainBlockHashes', (accounts) => {
   let superuser = accounts[0]
@@ -63,6 +72,8 @@ contract('ChainBlockHashes', (accounts) => {
         if (c !== 3) {
           await contracts[c].addChain(4, kovanGenesis, 'Kovan', { from: superuser })
         }
+
+        // each chain should have all but itself
         (await contracts[c].getChainCount()).toNumber().should.be.eq(chainCount - 1)
       }
       contracts.length.should.be.eq(chainCount)
@@ -83,6 +94,68 @@ contract('ChainBlockHashes', (accounts) => {
           }
         }
       }
+    })
+
+    it('should fail to re-add previous blocks', async () => {
+      await expectThrow(contracts[0].addBlock(2, 15122,
+        '0x12616f69e1fed7eabfd7b87bf2bdccb012616f69e1fed7eabfd7b87bf2bdccb0',
+        { from: accounts[1] }))
+    })
+
+    it('should have correct highest block numbers', async () => {
+      // chain 0 does not have itself
+      await expectThrow(contracts[0].getHighestBlockNumber(1));
+      (await contracts[1].getHighestBlockNumber(1)).toNumber().should.be.eq(23155);
+      (await contracts[2].getHighestBlockNumber(1)).toNumber().should.be.eq(23151);
+      (await contracts[3].getHighestBlockNumber(1)).toNumber().should.be.eq(23156)
+    })
+
+    it('should have matchy block hashes', async () => {
+      (await contracts[1].getBlockHash(1, 23151)).should.be.eq(
+        await contracts[2].getBlockHash(1, 23151));
+      (await contracts[2].getBlockHash(1, 23151)).should.be.eq(
+        await contracts[3].getBlockHash(1, 23151));
+      (await contracts[1].getBlockHash(1, 23154)).should.be.eq(
+        await contracts[3].getBlockHash(1, 23154));
+      (await contracts[1].getBlockHash(1, 23155)).should.be.eq(
+        await contracts[3].getBlockHash(1, 23155));
+      (await contracts[0].getBlockHash(2, 15126)).should.be.eq(
+        await contracts[2].getBlockHash(2, 15126));
+      (await contracts[0].getBlockHash(3, 8847)).should.be.eq(
+        await contracts[1].getBlockHash(3, 8847));
+      (await contracts[0].getBlockHash(4, 28244)).should.be.eq(
+        await contracts[2].getBlockHash(4, 28244))
+    })
+
+    it('should not return hashes for unrecorded blocks', async () => {
+      await expectThrow(contracts[0].getBlockHash(2, 15128))
+      await expectThrow(contracts[1].getBlockHash(1, 23153))
+      await expectThrow(contracts[2].getBlockHash(1, 23153))
+      await expectThrow(contracts[3].getBlockHash(1, 23157))
+    })
+
+    // The intention here is to avoid even the appearance of retconning;
+    // block numbers only go up.
+    it('should not allow previous blocks to be inserted', async () => {
+      (await contracts[0].getHighestBlockNumber(3)).toNumber().should.be.gt(8845)
+      // sanity test on our fakeHash function
+      fakeHash(8845).should.eq('0xff82db7535530637af7f8a96284b3459ff82db7535530637af7f8a96284b3459')
+      await expectThrow(contracts[0].addBlock(3, 8845, fakeHash(8845), { from: accounts[1] }))
+    })
+
+    it('should allow additional valid blocks', async () => {
+      await contracts[0].addBlock(2, 15128, fakeHash(15128), { from: accounts[1] })
+      await contracts[0].addBlock(3, 8853, fakeHash(8853), { from: accounts[1] })
+      await contracts[0].addBlock(4, 28250, fakeHash(28250), { from: accounts[1] })
+      await contracts[1].addBlock(1, 23156, fakeHash(23156), { from: accounts[2] })
+      await contracts[1].addBlock(3, 8852, fakeHash(8852), { from: accounts[2] })
+      await contracts[1].addBlock(4, 28249, fakeHash(28249), { from: accounts[2] })
+      await contracts[2].addBlock(1, 23152, fakeHash(23152), { from: accounts[3] })
+      await contracts[2].addBlock(2, 15127, fakeHash(15127), { from: accounts[3] })
+      await contracts[2].addBlock(4, 28245, fakeHash(28245), { from: accounts[3] })
+      await contracts[3].addBlock(1, 23157, fakeHash(23157), { from: accounts[4] })
+      await contracts[3].addBlock(2, 15122, fakeHash(15122), { from: accounts[4] })
+      await contracts[3].addBlock(3, 8844, fakeHash(8844), { from: accounts[4] })
     })
   })
 })
