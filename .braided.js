d58f4823df9b03451537ec59cd6c03a370bdd5e0
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-10-11 15:51:14 +0200

    rename write -> watch

--- .braided.js
+++ .braided.js
@@ -12,26 +12,26 @@ module.exports = {
     // Record one block per day from Ropsten on Mainnet
     { chain: 'mainnet',
       braidedContract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
-      write: [
+      watch: [
         { chain: 'ropsten', blocks: 0, seconds: 86400 }] },
     // Record every single block from Mainnet, Rinkeby and Kovan on Ropsten
     { chain: 'ropsten',
       braidedContract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
-      write: [
+      watch: [
         { chain: 'mainnet', blocks: 0, seconds: 0 },
         { chain: 'rinkeby', blocks: 0, seconds: 0 },
         { chain: 'kovan', blocks: 0, seconds: 0 }] },
     // Record every single block from Mainnet, Ropsten and Kovan on Rinkeby
     { chain: 'rinkeby',
       braidedContract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
-      write: [
+      watch: [
         { chain: 'mainnet', blocks: 0, seconds: 0 },
         { chain: 'ropsten', blocks: 0, seconds: 0 },
         { chain: 'kovan', blocks: 0, seconds: 0 }] },
     // Record every single block from Mainnet, Ropsten and Kovan on Kovan
     { chain: 'kovan',
       braidedContract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
-      write: [
+      watch: [
         { chain: 'mainnet', blocks: 0, seconds: 0 },
         { chain: 'ropsten', blocks: 0, seconds: 0 },
         { chain: 'rinkeby', blocks: 0, seconds: 0 }] }
