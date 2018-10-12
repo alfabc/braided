d05b239eaf041f0c1aa6be48a3a04311ab090d83
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-10-12 15:35:04 +0200

    reorganize config, instantiate contract interfaces

--- .braided.js
+++ .braided.js
@@ -1,47 +1,55 @@
 module.exports = {
   // Chains are public blockchains
-  chains: [
-    { id: 'mainnet', client: 'geth', networkID: 1, clientChainName: '' },
-    { id: 'ropsten', client: 'geth', networkID: 3, clientChainName: 'testnet' },
-    { id: 'rinkeby', client: 'geth', networkID: 4, clientChainName: 'rinkeby' },
-    { id: 'kovan', client: 'parity', networkID: 42, clientChainName: 'kovan' }],
+  chains: {
+    mainnet: { client: 'geth', networkID: 1, clientChainName: '' },
+    ropsten: { client: 'geth', networkID: 3, clientChainName: 'testnet' },
+    rinkeby: { client: 'geth', networkID: 4, clientChainName: 'rinkeby' },
+    kovan: { client: 'parity', networkID: 42, clientChainName: 'kovan' } },
 
   // Strands are deployed Braided contracts on public blockchains
   // There can be multiple strands deployed on a single chain; old versions,
   // abandoned, competing, forked, etc.
-  strands: [
-    { chain: 'mainnet', contract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
-    { chain: 'ropsten', contract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
-    { chain: 'rinkeby', contract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
-    { chain: 'kovan', contract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
-  ],
+  strands: {
+    mainnet0: {
+      chain: 'mainnet',
+      contractAddress: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
+    ropsten0: {
+      chain: 'ropsten',
+      contractAddress: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
+    rinkeby0: {
+      chain: 'rinkeby',
+      contractAddress: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
+    kovan0: {
+      chain: 'kovan',
+      contractAddress: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' }
+  },
 
   // write chain, [read chains, block count interval, seconds interval]
   // interval "0" means every block or every second (no delay)
   // both apply; longest of the two prevails
   agents: [
-    // Record one block per day from Ropsten on Mainnet
-    { chain: 'mainnet',
-      braidedContract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
-      watch: [
-        { chain: 'ropsten', blocks: 0, seconds: 86400 }] },
-    // Record every single block from Mainnet, Rinkeby and Kovan on Ropsten
-    { chain: 'ropsten',
-      braidedContract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
+    // Record one block per day from Ropsten on the mainnet0 strand
+    // { strand: 'mainnet0',
+    //   address: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
+    //   watch: [
+    //     { chain: 'ropsten', blocks: 0, seconds: 86400 }] },
+    // Record every block from Mainnet, Rinkeby and Kovan on the ropsten0 strand
+    { strand: 'ropsten0',
+      address: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
       watch: [
         { chain: 'mainnet', blocks: 0, seconds: 0 },
         { chain: 'rinkeby', blocks: 0, seconds: 0 },
         { chain: 'kovan', blocks: 0, seconds: 0 }] },
-    // Record every single block from Mainnet, Ropsten and Kovan on Rinkeby
-    { chain: 'rinkeby',
-      braidedContract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
+    // Record every block from Mainnet, Ropsten and Kovan on the rinkeby0 strand
+    { strand: 'rinkeby0',
+      address: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
       watch: [
         { chain: 'mainnet', blocks: 0, seconds: 0 },
         { chain: 'ropsten', blocks: 0, seconds: 0 },
         { chain: 'kovan', blocks: 0, seconds: 0 }] },
-    // Record every single block from Mainnet, Ropsten and Kovan on Kovan
-    { chain: 'kovan',
-      braidedContract: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
+    // Record every block from Mainnet, Ropsten and Kovan on the kovan0 strand
+    { strand: 'kovan0',
+      address: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
       watch: [
         { chain: 'mainnet', blocks: 0, seconds: 0 },
         { chain: 'ropsten', blocks: 0, seconds: 0 },
--- bin/exec.js
+++ bin/exec.js
@@ -8,8 +8,11 @@ const Web3 = require('web3')
 
 const config = reqCwd.silent('./.braided.js') || {}
 
+const braidedArtifacts = require('../build/contracts/Braided.json')
+
 let clients = {}
 let contracts = {}
+let strands = {}
 let web3s = {}
 
 death((signal, err) => cleanUp())
@@ -17,26 +20,36 @@ death((signal, err) => cleanUp())
 // launch an agent for each listed in the config
 launchClients()
 
+// create contract and instance for each contract on each strand
+for (let key in config.strands) {
+  let strand = config.strands[key]
+  contracts[key] = contract(braidedArtifacts)
+  contracts[key].setProvider(web3s[strand.chain].currentProvider)
+  contracts[key].defaults({ gas: '250000' })
+  strands[key] = contracts[key].at(strand.contractAddress)
+}
+
 // for each agent, add the appropriate watchers for each chain
 // according to the config
 for (let agent of config.agents) {
   console.log(agent)
-  console.log(agent.chain)
-  console.log(clients[agent.chain])
+  console.log(agent.strand)
+  // console.log(clients[agent.chain])
 }
 
 // launch a client for each chain listed in the config;
 // each includes a Geth/Parity config
 function launchClients () {
   return new Promise((resolve, reject) => {
-    for (let chain of config.chains) {
+    for (let key in config.chains) {
+      let chain = config.chains[key]
       let params = ''
       let port = 30700 + chain.networkID
       let rpcport = 3370 + chain.networkID
       if (chain.client === 'geth') {
         params = `--port ${port} --rpc --rpcaddr "0.0.0.0" --rpcport ${rpcport} --rpcapi "web3,eth,net,debug" --rpccorsdomain "*" --syncmode "light" --${chain.clientChainName}` // eslint-disable-line max-len
       } else if (chain.client === 'parity') {
-        params = `--light --port=${port} --jsonrpc-port=${rpcport} --chain=${chain.id}`
+        params = `--light --port=${port} --jsonrpc-port=${rpcport} --chain=${key}`
       } else {
         console.log(`Configuration error: Unsupported client '${chain.client}'`)
         return (1)
@@ -44,11 +57,13 @@ function launchClients () {
       let proc = childprocess.exec(`${chain.client} ${params}`, (error, stdout, stderr) => {
         if (error);
       })
-      console.log(`Spawned ${chain.client} pid ${proc.pid} for ${chain.id}`)
-      clients[chain.id] = proc
+      console.log(`Spawned ${chain.client} pid ${proc.pid} for ${key}`)
+      clients[key] = proc
 
       // create a Web3 instance for each client
-      web3s[chain.id] = new Web3(new Web3.providers.HttpProvider("http://localhost:" + rpcport))
+      web3s[key] = new Web3(new Web3.providers.HttpProvider('http://localhost:' + rpcport))
+
+      // add a watcher for new blocks
     }
   })
 }
