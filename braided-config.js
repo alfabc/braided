e7ff1f6dd0a75bce54c592cb883a75b263bccb5b
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-10-25 16:02:12 +0200

    fix npm craziness

--- braided-config.js
+++ braided-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   // Chains are public blockchains
   chains: {
@@ -15,16 +17,16 @@ module.exports = {
   strands: {
     mainnet0: {
       chain: 'mainnet',
-      contractAddress: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
+      contractAddress: '0x0000000000000000000000000000000000000000' },
     ropsten0: {
       chain: 'ropsten',
-      contractAddress: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
+      contractAddress: '0x6a8b46Ce6d0F629a82ab97E580c8Ea3100bE3ffE' },
     rinkeby0: {
       chain: 'rinkeby',
-      contractAddress: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' },
+      contractAddress: '0x89281174670f3674c509eF935391168eFB5095d9' },
     kovan0: {
       chain: 'kovan',
-      contractAddress: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f' }
+      contractAddress: '0x0000000000000000000000000000000000000000' }
   },
 
   // Describes the agents which have permission to add block/hashes to
@@ -35,29 +37,29 @@ module.exports = {
   agents: [
     // Record one block per day from Ropsten on the mainnet0 strand
     // { strand: 'mainnet0',
-    //   address: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
+    //   address: process.env.BRAIDED_MAINNET_AGENT,
     //   watch: [
     //     { chain: 'ropsten', blocks: 0, seconds: 86400 }] },
     // Record blocks from Mainnet, Rinkeby and Kovan on the ropsten0 strand
     { strand: 'ropsten0',
-      address: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
+      address: process.env.BRAIDED_ROPSTEN_AGENT,
       watch: {
         mainnet: { blocks: 1, seconds: 10 },
         rinkeby: { blocks: 2, seconds: 20 },
         kovan: { blocks: 3, seconds: 30 } } },
     // Record blocks from Mainnet, Ropsten and Kovan on the rinkeby0 strand
     { strand: 'rinkeby0',
-      address: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
+      address: process.env.BRAIDED_RINKEBY_AGENT,
       watch: {
         mainnet: { blocks: 1, seconds: 11 },
         ropsten: { blocks: 2, seconds: 21 },
-        kovan: { blocks: 3, seconds: 31 } } },
+        kovan: { blocks: 3, seconds: 31 } } }
     // Record blocks from Mainnet, Ropsten and Rinkeby on the kovan0 strand
-    { strand: 'kovan0',
-      address: '0x00C8Bc664147389328Cb56f0b1EDc391c591191f',
-      watch: {
-        mainnet: { blocks: 1, seconds: 12 },
-        ropsten: { blocks: 2, seconds: 22 },
-        rinkeby: { blocks: 3, seconds: 32 } } }
+    // { strand: 'kovan0',
+    //   address: process.env.BRAIDED_KOVAN_AGENT,
+    //   watch: {
+    //     mainnet: { blocks: 1, seconds: 12 },
+    //     ropsten: { blocks: 2, seconds: 22 },
+    //     rinkeby: { blocks: 3, seconds: 32 } } }
   ]
 }
