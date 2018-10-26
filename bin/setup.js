952eca09a7e9de31d209f32b8450da623d110d6c
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-10-26 15:00:56 +0200

    setup works, but doesn't exit.

--- bin/setup.js
+++ bin/setup.js
@@ -1,40 +1,50 @@
 #!/usr/bin/env node
 const contract = require('truffle-contract')
+const HDWalletProvider = require('truffle-hdwallet-provider')
 const Web3 = require('web3')
 
 const config = require('../braided-config.js')
 
 const braidedArtifacts = require('../build/contracts/Braided.json')
 
-let clients = {}
-let contracts = {}
+setup()
 
-// iterate over the agents that need to be set up
-for (let agent of config.agents) {
-  // Each agent operates on a braid
-  console.log(agent.braid)
-  // the agent's address
-  console.log(agent.agentAddress)
-  // Each braid has a contract address
-  console.log(config.braids[agent.braid].contractAddress)
-  // and we'll need the owner's mnemonic
-  console.log(config.braids[agent.braid].ownerMnemonic)
-  // Iterate over the chains watched for each agent
-  for (let key in agent.watches) {
-    // The chain watched
-    console.log(key)
-    let watch = agent.watches[key]
-    // add a strand for the chain
-    // thisBraid.addStrand(watch.strand,
-    //   config.braids[agent.braid].contractAddress,
-    //   config.braids[agent.braid].genesisBlockHash,
-    //   config.braids[agent.braid].chain)
-    console.log(`thisBraid.addStrand(${watch.strand},
-    ${config.braids[agent.braid].contractAddress},
-    ${config.braids[agent.braid].genesisBlockHash},
-    ${config.braids[agent.braid].chain})`)
-    // give the agent permission to write to the strand
-    // thisBraid.addAgent(agent.address, watch.strand)
-    console.log(`thisBraid.addAgent(${agent.address}, ${watch.strand})`)
-  }
+function setup () {
+  return new Promise(async () => {
+    // iterate over the agents that need to be set up
+    for (let agent of config.agents) {
+      // Each agent operates on a braid
+      console.log(agent.braid)
+      // the agent's address
+      console.log(agent.agentAddress)
+      // Each braid has a contract address
+      console.log(config.braids[agent.braid].contractAddress)
+      // and we'll need the owner's mnemonic
+      console.log(config.braids[agent.braid].ownerMnemonic)
+      // make a web3 connection to the chain that holds the braid
+      let web3 = new Web3(
+        new HDWalletProvider(config.braids[agent.braid].ownerMnemonic,
+         config.braids[agent.braid].providerEndpoint))
+      let braidedContract = contract(braidedArtifacts)
+      braidedContract.setProvider(web3.currentProvider)
+      braidedContract.defaults({ gas: '250000' })
+      let braid = braidedContract.at(config.braids[agent.braid].contractAddress)
+
+      // Iterate over the chains watched for each agent
+      for (let key in agent.watches) {
+        // The chain watched
+        console.log(`adding ${key} to ${agent.braid}`)
+        let watch = agent.watches[key]
+        // add a strand for the chain
+        console.log(`addStrand(${watch.strand}, ${config.braids[key].contractAddress}, ${config.braids[key].genesisBlockHash}, ${config.braids[key].chain})`)
+//        await braid.addStrand(watch.strand,
+//          config.braids[key].contractAddress,
+//          config.braids[key].genesisBlockHash,
+//          config.braids[key].chain)
+        // give the agent permission to write to the strand
+        console.log(`addAgent(${agent.agentAddress}, ${watch.strand})`)
+//        await braid.addAgent(agent.agentAddress, watch.strand)
+      }
+    }
+  })
 }
--- braided-config.js
+++ braided-config.js
@@ -15,36 +15,41 @@ module.exports = {
   // There can be multiple strands deployed on a single chain; old versions,
   // abandoned, competing, forked, etc.
   braids: {
-    mainnet0: {
+    mainnet: {
       chain: 'mainnet',
       contractAddress: '0x0000000000000000000000000000000000000000',
       genesisBlockHash: '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
       ownerAddress: '0x5160b2f588debd7d131a8f8b4649bf9ac7bfcd59',
-      ownerMnemonic: process.env.BRAIDED_MAINNET_OWNER },
-    morden0: {
+      ownerMnemonic: process.env.BRAIDED_MAINNET_OWNER,
+      providerEndpoint: process.env.INFURA_MAINNET },
+    morden: {
       chain: 'morden',
       genesisBlockHash: '0x0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303',
       contractAddress: '0x0000000000000000000000000000000000000000',
       ownerAddress: '0x0000000000000000000000000000000000000000',
-      ownerMnemonic: ''},
-    ropsten0: {
+      ownerMnemonic: '',
+      providerEndpoint: '' },
+    ropsten: {
       chain: 'ropsten',
       contractAddress: '0x6a8b46Ce6d0F629a82ab97E580c8Ea3100bE3ffE',
       genesisBlockHash: '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d',
       ownerAddress: '0xb0e5a65bc680659da0715314f96de6b0646baeb3',
-      ownerMnemonic: process.env.BRAIDED_ROPSTEN_OWNER },
-    rinkeby0: {
+      ownerMnemonic: process.env.BRAIDED_ROPSTEN_OWNER,
+      providerEndpoint: process.env.INFURA_ROPSTEN },
+    rinkeby: {
       chain: 'rinkeby',
       contractAddress: '0x2eF96AEeadAb8C8005c83A1499388F01646D11b0',
       genesisBlockHash: '0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177',
       ownerAddress: '0xf0bf12fa12A78402f821E61b02eE28a186192f9A',
-      ownerMnemonic: process.env.BRAIDED_RINKEBY_OWNER },
-    kovan0: {
+      ownerMnemonic: process.env.BRAIDED_RINKEBY_OWNER,
+      providerEndpoint: process.env.INFURA_RINKEBY },
+    kovan: {
       chain: 'kovan',
       contractAddress: '0x0000000000000000000000000000000000000000',
       genesisBlockHash: '0xa3c565fc15c7478862d50ccd6561e3c06b24cc509bf388941c25ea985ce32cb9',
       ownerAddress: '0x36feec403513a07239f3a7e4f6751a7e651e330a',
-      ownerMnemonic: process.env.BRAIDED_KOVAN_OWNER }
+      ownerMnemonic: process.env.BRAIDED_KOVAN_OWNER,
+      providerEndpoint: process.env.INFURA_KOVAN }
   },
 
   // Describes the agents which have permission to add block/hashes to
@@ -54,13 +59,13 @@ module.exports = {
   // both apply; longest of the two prevails
   agents: [
     // Record one block per day from Ropsten on the mainnet0 strand
-    // { braid: 'mainnet0',
+    // { braid: 'mainnet',
     //   agentAddress: '0x826803b0f74c279faf4e327f02f7b8fbb751607d',
     //   agentMnemonic: process.env.BRAIDED_MAINNET_AGENT,
     //   watches: {
     //     ropsten: { strand: 3, blocks: 0, seconds: 86400 } } },
     // Record blocks from Mainnet, Rinkeby and Kovan on the ropsten0 strand
-    { braid: 'ropsten0',
+    { braid: 'ropsten',
       agentAddress: '0x10a259146c4ac177a74d17591bf83739587a219d',
       agentMnemonic: process.env.BRAIDED_ROPSTEN_AGENT,
       watches: {
@@ -68,7 +73,7 @@ module.exports = {
         rinkeby: { strand: 4, blocks: 2, seconds: 20 },
         kovan: { strand: 42, blocks: 3, seconds: 30 } } },
     // Record blocks from Mainnet, Ropsten and Kovan on the rinkeby0 strand
-    { braid: 'rinkeby0',
+    { braid: 'rinkeby',
       agentAddress: '0xa4a8c40cf200e548305001b9af9965722c70c6ad',
       agentMnemonic: process.env.BRAIDED_RINKEBY_AGENT,
       watches: {
@@ -76,7 +81,7 @@ module.exports = {
         ropsten: { strand: 3, blocks: 2, seconds: 21 },
         kovan: { strand: 42, blocks: 3, seconds: 31 } } },
     // Record blocks from Mainnet, Ropsten and Rinkeby on the kovan0 strand
-    { braid: 'kovan0',
+    { braid: 'kovan',
       agentAddress: '0x4487f27ad58abbf8f3b25ee38aba578bc979f67e',
       agentMnemonic: process.env.BRAIDED_KOVAN_AGENT,
       watches: {
