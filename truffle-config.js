43d3b8bfe40445803e1f35a2dd9733d204be9cab
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-11-09 21:05:26 +0100

    Truffle is not as stupid as I thought. https://github.com/trufflesuite/truffle/releases/tag/v5.0.0-beta.0#bring-your-own-compiler

--- truffle-config.js
+++ truffle-config.js
@@ -2,6 +2,11 @@ const HDWalletProvider = require('truffle-hdwallet-provider')
 require('dotenv').config()
 
 module.exports = {
+  compilers: {
+     solc: {
+       version: "0.4.25"
+     }
+  },
   solc: {
     optimizer: {
       enabled: true,
